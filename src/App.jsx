731e5e4cfd21b69af0e8e63e57ba7831d80b065b
roy-biplobe <biplobkumargk@gmail.com>
2025-01-15 13:39:56 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import SnakeGame from "./components/SnakeGame"
+
+function App() {
+  return (
+  <SnakeGame></SnakeGame>
+  )
+}
+
+export default App
---
+++ src/components/SnakeGame.jsx
@@ -0,0 +1,9 @@
+const SnakeGame = () => {
+ return (
+ <div>
+ <h2>Welcome to the SnakeGame page</h2>
+ </div>
+ );
+};
+
+export default SnakeGame;
